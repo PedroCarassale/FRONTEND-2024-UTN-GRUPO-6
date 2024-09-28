8e5d6bd5b5059333dc3989414e23123363e959d0
Pedro Carassale <pedrocarassale@gmail.com>
2024-09-28 13:36:58 -0300

    HomePage primeros pasos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="app">
+      <h2 className="mis-gimnasios">Mis Gimnasios</h2>
+      <div className="app-cards">
+        <Card
+          img="src/assets/gym.jpg"
+          title="Gym"
+          description="Please add your content here. Keep it short and simple. And smile :)"
+          tags={[
+            ["#f00", "Title"],
+            ["#f00", "Title"],
+          ]}
+        ></Card>
+        <Card
+          img="src/assets/gym.jpg"
+          title="Gym"
+          description="Please add your content here. Keep it short and simple. And smile :)"
+          tags={[
+            ["#f00", "Title"],
+            ["#f00", "Title"],
+          ]}
+        ></Card>
+        <Card
+          img="src/assets/gym.jpg"
+          title="Gym"
+          description="Please add your content here. Keep it short and simple. And smile :)"
+          tags={[
+            ["#f00", "Title"],
+            ["#f00", "Title"],
+          ]}
+        ></Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
