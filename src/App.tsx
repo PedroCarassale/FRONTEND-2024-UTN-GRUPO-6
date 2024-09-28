0b36963e8f73299c73e57b8f9d61ca76302953a4
Pedro Carassale <pedrocarassale@gmail.com>
2024-09-28 14:24:08 -0300

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import "./App.css";
 import Card from "./components/Card";
 
+function getCards(count: number) {
+  return Array.from({ length: count }, (_, index) => (
+    <Card
+      img="src/assets/gym.jpg"
+      title="Gym"
+      description="Please add your content here. Keep it short and simple. And smile :)"
+      tags={[
+        ["#f00", "Title"],
+        ["#f00", "Title"],
+      ]}
+    ></Card>
+  ));
+}
+
 function App() {
   return (
     <section className="app">
+      {/* Mis Gimnasios */}
       <h2 className="mis-gimnasios">Mis Gimnasios</h2>
-      <div className="app-cards">
-        <Card
-          img="src/assets/gym.jpg"
-          title="Gym"
-          description="Please add your content here. Keep it short and simple. And smile :)"
-          tags={[
-            ["#f00", "Title"],
-            ["#f00", "Title"],
-          ]}
-        ></Card>
-        <Card
-          img="src/assets/gym.jpg"
-          title="Gym"
-          description="Please add your content here. Keep it short and simple. And smile :)"
-          tags={[
-            ["#f00", "Title"],
-            ["#f00", "Title"],
-          ]}
-        ></Card>
-        <Card
-          img="src/assets/gym.jpg"
-          title="Gym"
-          description="Please add your content here. Keep it short and simple. And smile :)"
-          tags={[
-            ["#f00", "Title"],
-            ["#f00", "Title"],
-          ]}
-        ></Card>
-      </div>
+      <div className="app-explorar-gimnasios-cards">{getCards(3)}</div>
+
+      {/* Explorar Gimnasios */}
+      <h2 className="mis-gimnasios">Explorar Gimnasios</h2>
+      <div className="app-explorar-gimnasios-cards">{getCards(10)}</div>
     </section>
   );
 }
