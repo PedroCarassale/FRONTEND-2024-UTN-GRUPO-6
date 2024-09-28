e05ce3b8f7933f0ecd31d2128a066a6a1ba6ea67
Pedro Carassale <pedrocarassale@gmail.com>
2024-09-28 17:37:00 -0300

    BrowserRouter done

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import "./App.css";
-import Card from "./components/Card";
-
-function getCards(count: number) {
-  return Array.from({ length: count }, (_, index) => (
-    <Card
-      img="src/assets/gym.jpg"
-      title="Gym"
-      description="Please add your content here. Keep it short and simple. And smile :)"
-      tags={[
-        ["#f00", "Title"],
-        ["#f00", "Title"],
-      ]}
-    ></Card>
-  ));
-}
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <section className="app">
-      {/* Mis Gimnasios */}
-      <h2 className="mis-gimnasios">Mis Gimnasios</h2>
-      <div className="app-explorar-gimnasios-cards">{getCards(3)}</div>
-
-      {/* Explorar Gimnasios */}
-      <h2 className="mis-gimnasios">Explorar Gimnasios</h2>
-      <div className="app-explorar-gimnasios-cards">{getCards(10)}</div>
-    </section>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import "./Home.css";
+import Card from "../components/Card";
+
+function getCards(count: number) {
+  return Array.from({ length: count }, (_) => (
+    <Card
+      img="src/assets/gym.jpg"
+      title="Gym"
+      description="Please add your content here. Keep it short and simple. And smile :)"
+      tags={[
+        ["#f00", "Title"],
+        ["#f00", "Title"],
+      ]}
+    ></Card>
+  ));
+}
+function Home() {
+  return (
+    <section className="app">
+      {/* Mis Gimnasios */}
+      <h2 className="mis-gimnasios">Mis Gimnasios</h2>
+      <div className="app-explorar-gimnasios-cards">{getCards(3)}</div>
+
+      {/* Explorar Gimnasios */}
+      <h2 className="mis-gimnasios">Explorar Gimnasios</h2>
+      <div className="app-explorar-gimnasios-cards">{getCards(10)}</div>
+    </section>
+  );
+}
+
+export default Home;
