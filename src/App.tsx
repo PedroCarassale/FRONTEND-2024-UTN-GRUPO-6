89765d654670a3a01461f5089798843798075e9e
Sebastian Carena <sebastian.carena1902@gmail.com>
2024-10-01 21:06:26 -0300

    bootstrap+carrusel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Card from "./components/Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function getCards(count: number) {
-  return Array.from({ length: count }, (_, index) => (
-    <Card
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      {Array.from({ length: count }, (_, index) => (
+     <Card
+      key={index}
       img="src/assets/gym.jpg"
       title="Gym"
       description="Please add your content here. Keep it short and simple. And smile :)"
@@ -11,8 +17,10 @@ function getCards(count: number) {
         ["#f00", "Title"],
         ["#f00", "Title"],
       ]}
-    ></Card>
-  ));
+      ></Card>
+      ))}
+      </div>
+  );
 }
 
 function App() {
@@ -20,7 +28,18 @@ function App() {
     <section className="app">
       {/* Mis Gimnasios */}
       <h2 className="mis-gimnasios">Mis Gimnasios</h2>
-      <div className="app-explorar-gimnasios-cards">{getCards(3)}</div>
+
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          {getCards(3)}
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          {getCards(3)}
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          {getCards(3)}
+        </Carousel.Item>
+      </Carousel>
 
       {/* Explorar Gimnasios */}
       <h2 className="mis-gimnasios">Explorar Gimnasios</h2>
