37343d11615457fd334cb9ae6f917f7c2f612a8e
rodri <rodrigoramirezok@hotmail.com>
2024-10-18 13:24:23 -0300

    Se agregó boton de iniciar sesion

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,23 +7,10 @@ export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        {/* Logo */}
         <a className="navbar-brand" href="/">
-          <img className="logo" src="/Logo-DDS-Circular.png" alt="Logo" />
+        <img className="logo" src="/Logo-DDS-Circular.png" alt="Logo" />
         </a>
-
-        {/* Enlaces Home y Link */}
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" href="#">
@@ -42,48 +29,59 @@ export default function NavBar() {
             </li>
           </ul>
 
-          {/* Buscador en el centro */}
-          <form className="d-flex mx-auto" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Buscar"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Buscar
-            </button>
+        {/* Botón para mostrar/ocultar el menú en dispositivos móviles */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Buscador */}
+          <form className="d-flex ms-auto">
+            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
+            <button className="btn btn-outline-light" type="submit">Buscar</button>
           </form>
 
-          {/* Dropdown de usuario */}
-          <div className="dropdown ms-auto">
-            <button className="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i className="bi bi-person-circle"></i>
-            </button>
+          {/*Mostrar botón de login*/} 
+            <a className="btn btn-outline-light ms-3" href="/login">
+              Iniciar sesión
+            </a>
 
-            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Cuenta
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Configuración
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Cerrar sesión
-                </a>
-              </li>
-            </ul>
-          </div>
+          {/*Icono de user + dropdown según si está logueado */}
+            {/*<div className="dropdown ms-3">
+              <button
+                className="btn btn-outline-light dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="bi bi-person-circle"></i>
+              </button>
+
+              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Cuenta
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Configuración
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#" onClick={logout}>
+                    Cerrar sesión
+                  </a>
+                </li>
+              </ul>
+            </div>*/}
         </div>
       </div>
     </nav>
   );
 }
+
