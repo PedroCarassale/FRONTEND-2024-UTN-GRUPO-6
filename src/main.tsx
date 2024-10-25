dd9c3a213d1c96c0d59b540170fdff6449b44341
Pedro Carassale <pedrocarassale@gmail.com>
2024-10-25 10:27:41 -0300

    code formatted

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import NavBar from "./components/NavBar.tsx";
 
-
 const root = createRoot(document.getElementById("root")!);
 root.render(
-
-  <><header>
-    <NavBar />
-  </header><>
+  <>
+    <header>
+      <NavBar />
+    </header>
+    <>
       <App />
     </>
-    </>
+  </>
 );
